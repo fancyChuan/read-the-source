ab106cd6209cc11713b4ae67e4f91aca156cc3bb
fancyChuan <1247375074@qq.com>
2019-05-29 00:06:51 +0800

    YARN：自定义RPC的client实现

---
+++ hadoop/src/rpc/MyRPCClient.java
@@ -0,0 +1,22 @@
+package rpc;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.ipc.RPC;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class MyRPCClient {
+    public static void main(String[] args) throws IOException {
+        ClientProtocol proxy = (ClientProtocol) RPC.getProxy(
+                ClientProtocol.class,
+                ClientProtocol.versionID,
+                new InetSocketAddress("localhost",6666),
+                new Configuration());
+
+        int result = proxy.add(5, 6);
+        String xxx = proxy.echo("xxx");
+        System.out.println(result);
+        System.out.println(xxx);
+    }
+}
